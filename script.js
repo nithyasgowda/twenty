499faf1397857023847f6be4251d231cc123ea06
Nithyashree S <nithyasgowda622000@gmail.com>
2025-04-24 12:21:25 +0530

    optimization

--- script.js
+++ script.js
@@ -1,42 +1,12 @@
-// console.log("hii")
-// let check=false;
-// document.getElementById("burger").addEventListener('click', function(e){
-//     const nav=document.querySelector('nav')
-//     const wrapper=document.querySelector('wrapper')
-//     if(!check){
-//         this.style.left="300px"
-//         nav.style.cssText="display:block;right:0;"
-//         console.log(nav);
-//         check=!check;
-//     }else{
-//         this.style.left="15px"
-//         nav.style.display="none"
-//         console.log(nav);
-//         check=!check;
-//     }
-
-//     const wrapper=document.getElementById("wrapper")
-//     console.log(wrapper)
-//     // wrapper.style.marginLeft="300px"
-// })
-
-
-let check = false;
-
-document.getElementById("burger").addEventListener('click', function (e) {
-    e.preventDefault();
-    const nav = document.querySelector('#nav_panel');
-    const wrapper = document.querySelector('#wrapper');
-
-    if (!check) {
-        nav.classList.add('slide-in');
-        nav.classList.remove('slide-out');
-        wrapper.classList.toggle('nav-open')
-        check = true;
-    } else {
-        nav.classList.add('slide-out');
-        nav.classList.remove('slide-in');
-        wrapper.classList.toggle('nav-open');
-        check = false;
-    }
-})
+function navSlider() {
+  const navPanel = document.querySelector("#nav_panel");
+  const wrapper = document.querySelector("#wrapper");
+  document.getElementById("burger").addEventListener("click", function () {
+    const isOpen = navPanel.classList.contains("slide-in");
+    navPanel.classList.toggle("slide-in", !isOpen);
+    navPanel.classList.toggle("slide-out", isOpen);
+    wrapper.classList.toggle("nav-open");
+    this.style.transform = isOpen ? "translateX(0)" : "translateX(280px)";
+  });
+}
+navSlider();
